febba35ab6b4eaf130cd9be81320b860321e141b
hjck4433 <hjck4433@gmail.com>
2022-04-25 20:54:34 +0900

    Create project

---
+++ src/store/about.js
@@ -0,0 +1,10 @@
+export default {
+  namespaced: true,
+  state: () => ({
+    name: 'HYUN JI',
+    email: 'hjck4433@gmail.com',
+    blog: '',
+    phone: '+82-10-4433-5125',
+    image: 'https://heropy.blog/css/images/logo.png'
+  })
+}
