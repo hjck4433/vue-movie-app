cf422080c92d3f9b37ca2ac363b26df45a949fd8
HYUNJI <84636849+hjck4433@users.noreply.github.com>
2023-09-13 16:03:01 +0900

    Update movie.js

--- functions/movie.js
+++ functions/movie.js
@@ -6,8 +6,8 @@ exports.handler = async function(event) {
   const payload = JSON.parse(event.body)
   const { title, type, year, page, id} = payload
   const url = id 
-    ? `https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&i=${id}` 
-    : `https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&s=${title}&type=${type}&y=${year}&page=${page}`
+    ? `http://www.omdbapi.com/?i=tt3896198&apikey=${OMDB_API_KEY}&i=${id}` 
+    : `http://www.omdbapi.com/?i=tt3896198&apikey=${OMDB_API_KEY}&s=${title}&type=${type}&y=${year}&page=${page}`
 
   try {
     const { data } = await axios.get(url)
